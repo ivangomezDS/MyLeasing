bdde428225df9d86bad33fbffd515eaf47317229
Ivan Dario Gomez Cano <ivandario_ca@hotmail.com>
2019-08-31 13:26:48 -0500

    Delete Property

--- MyLeasing/MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing/MyLeasing.Web/Controllers/OwnersController.cs
@@ -465,6 +465,35 @@ namespace MyLeasing.Web.Controllers
             await _dataContext.SaveChangesAsync();
             return RedirectToAction($"{nameof(DetailsProperty)}/{contract.Property.Id}");
         }
+        public async Task<IActionResult> DeleteProperty(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _dataContext.Properties
+                .Include(p => p.Owner)
+                .Include(p => p.PropertyImages)
+                .Include(p => p.Contracts)
+                .FirstOrDefaultAsync(pi => pi.Id == id.Value);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            if (property.Contracts.Count != 0)
+            {
+                ModelState.AddModelError(string.Empty, "The property can´t  be deleted  becuase it  has contracts");
+                return RedirectToAction($"{nameof(Details)}/{property.Owner.Id}");
+            }
+            _dataContext.PropertyImages.RemoveRange(property.PropertyImages);
+            _dataContext.Contracts.RemoveRange(property.Contracts);
+            _dataContext.Properties.Remove(property);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction($"{nameof(Details)}/{property.Owner.Id}");
+        }
+
 
 
 
